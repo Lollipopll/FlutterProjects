92874f5b89e43582e11fb80af85e2ef62555960a
wangzx <wangzx@yomob.com>
2020-08-11 10:43:00 +0800

    style 格式化代码

--- lib/projects/page/pageview_page.dart
+++ lib/projects/page/pageview_page.dart
@@ -12,80 +12,87 @@ class PageViewPage extends StatelessWidget {
         appBar: AppBar(
           title: Text("PageView"),
         ),
-        body: Container(
-          height: 230,
-          child: PageView.builder(
-              scrollDirection: Axis.horizontal, // 滚动方向
-              controller: PageController(
-                // 控制器
-                initialPage: 1, //默认初始位置
-                viewportFraction: 0.9, //视察因子
-              ),
-              onPageChanged: (int index) {
-                // 滚动监听
-                print("当前page $index");
-              },
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                return PageItem(
-                  index: index,
-                  bgColor: bgColors[index],
-                  text: strings[index],
-                  onTap: () {
-                    showTipsDialog(context, strings[index],
-                        isIos: index % 2 == 0);
-                  },
-                );
-              }),
+        body: Column(
+          children: [
+            pageWidget(),
+          ],
         ));
   }
 
-  /// 展示Dialog弹窗
-  void showTipsDialog(BuildContext context, String content,
-      {bool isIos = false}) {
-    if (isIos) {
-      showCupertinoDialog(
-          context: context,
-          builder: (context) {
-            return CupertinoAlertDialog(
-              title: Text('这是title'),
-              content: Text(content),
-              actions: [
-                FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).pop('cancel');
-                    },
-                    child: Text('取消')),
-                FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).pop('ok');
-                    },
-                    child: Text('确定'))
-              ],
+  Widget pageWidget() {
+    return Container(
+      height: 230,
+      child: PageView.builder(
+          scrollDirection: Axis.horizontal, // 滚动方向
+          controller: PageController(
+            // 控制器
+            initialPage: 1, //默认初始位置
+            viewportFraction: 0.9, //视察因子
+          ),
+          onPageChanged: (int index) {
+            // 滚动监听
+            print("当前page $index");
+          },
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return PageItem(
+              index: index,
+              bgColor: bgColors[index],
+              text: strings[index],
+              onTap: () {
+                showTipsDialog(context, strings[index], isIos: index % 2 == 0);
+              },
             );
-          });
-    } else {
-      showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: Text('这是title'),
-              content: Text(content),
-              actions: [
-                FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).pop('cancel');
-                    },
-                    child: Text('取消')),
-                FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).pop('ok');
-                    },
-                    child: Text('确定'))
-              ],
-            );
-          });
-    }
+          }),
+    );
+  }
+}
+
+/// 展示Dialog弹窗
+void showTipsDialog(BuildContext context, String content,
+    {bool isIos = false}) {
+  if (isIos) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text('这是title'),
+            content: Text(content),
+            actions: [
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop('cancel');
+                  },
+                  child: Text('取消')),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop('ok');
+                  },
+                  child: Text('确定'))
+            ],
+          );
+        });
+  } else {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('这是title'),
+            content: Text(content),
+            actions: [
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop('cancel');
+                  },
+                  child: Text('取消')),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop('ok');
+                  },
+                  child: Text('确定'))
+            ],
+          );
+        });
   }
 }
 
